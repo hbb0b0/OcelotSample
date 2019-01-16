ad3194bcb28b5781408fd89197d3822b5224d868
hbb0b0@163.com <hbb0b0@163.com>
2019-01-16 17:19:38 +0800

    ocelotSample init

---
+++ Hbb0b0.Gateway/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.IO;
+namespace Hbb0b0.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile("Ocelot.json", true, true)
+
+                        // .AddOcelot(hostingContext.HostingEnvironment)
+                        .AddEnvironmentVariables();
+                })
+                .UseUrls("http://localhost:6000")
+                .UseStartup<Startup>();
+    }
+}
+
---
+++ Hbb0b0.Gateway/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+//using Ocelot.Provider.Consul;
+//using Ocelot.ConfigAuthLimitCache.DependencyInjection;
+//using Ocelot.ConfigAuthLimitCache.Middleware;
+
+namespace Hbb0b0.Gateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();//注入Ocelot服务
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseOcelot().Wait();//使用Ocelot中间件
+            app.UseMvc();
+        }
+    }
+}
